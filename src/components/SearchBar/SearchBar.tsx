ba5a8488f0860173e71f44fd574fe99fedc61377
miruwan <agou.ar1209@gmail.com>
2024-08-21 00:08:53 +0900

    feat: キーワードでアニメ情報を検索する処理を実装
     - submitでURLのパラメータに検索キーワードを設定
     - URLのパラメータから取得したキーワードでアニメ情報を取得

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,27 @@
+import { FormEvent, useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
 
 export function SearchBar() {
+  const router = useRouter()
+  const [keyword, setKeyword] = useState('')
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    router.push(`/?keyword=${keyword}`)
+  }
   return (
-    <>
+    <form onSubmit={onSubmit}>
       <InputGroup maxW={500}>
-        <Input placeholder="アニメのタイトルを入力 （例）鬼滅の刃" />
+        <Input
+          placeholder="アニメのタイトルを入力 （例）鬼滅の刃"
+          onChange={e => setKeyword(e.target.value)}
+        />
         <InputLeftElement>
           <SearchIcon color="gray.500" />
         </InputLeftElement>
       </InputGroup>
-    </>
+    </form>
   )
 }
