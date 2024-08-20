6a69994e9fe8819e1b389991c13f0cd9d0a5ab11
miruwan <agou.ar1209@gmail.com>
2024-08-21 00:25:12 +0900

    feat: 検索キーワードの初期化処理を追加
     - シーズン検索をした場合は検索バーの値を削除

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,21 +1,30 @@
-import { FormEvent, useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { FormEvent, useState, useEffect } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
 
 export function SearchBar() {
   const router = useRouter()
+  const searchParams = useSearchParams()
   const [keyword, setKeyword] = useState('')
+
+  useEffect(() => {
+    if (!searchParams.get('keyword')) {
+      setKeyword('')
+    }
+  }, [searchParams])
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault()
     router.push(`/?keyword=${keyword}`)
   }
+
   return (
     <form onSubmit={onSubmit}>
       <InputGroup maxW={500}>
         <Input
           placeholder="アニメのタイトルを入力 （例）鬼滅の刃"
+          value={keyword}
           onChange={e => setKeyword(e.target.value)}
         />
         <InputLeftElement>
