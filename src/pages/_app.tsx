0501dd0011c1604f56c8e9ae8ed0af596183270d
miruwan <agou.ar1209@gmail.com>
2024-09-29 16:34:33 +0900

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <ChakraProvider theme={customTheme}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/images/anime-search.png" />
         </Head>
 
         <DefaultSeo />
